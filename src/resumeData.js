086fccb6595c3713abff99a0b0f9b87e191a0f37
delmario89 <delgadomarioj@gmail.com>
2018-12-20 13:57:45 -0700

    Initial Commit

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Mario Delgado",
+    "role": "Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I am a self-taught React Developer, Frontend, Javascript, Html and CSS",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a Rock Climber who enjoys learning how to code.",
+    "address":"Denver, CO",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"",
+        "YearOfPassing":"",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"",
+        "YearOfPassing":"",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
